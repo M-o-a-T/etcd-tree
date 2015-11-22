28e8c7e7098bc5db0aff1f5fa53c20f204f116b1
Matthias Urlichs <matthias@urlichs.de>
2015-11-22 20:28:11 +0100

    * Add optional loop argument to factory

--- etctree/__init__.py
+++ etctree/__init__.py
@@ -42,13 +42,13 @@ import warnings
 warnings.filterwarnings('ignore', category=ResourceWarning)
 
 @asyncio.coroutine
-def client(cfg="/etc/etctree.cfg"):
+def client(cfg="/etc/etctree.cfg", loop=None):
 	if not isinstance(cfg,dict): # pragma: no branch
 		from .util import from_yaml
 		cfg = from_yaml(cfg)
 
 	from .etcd import EtcClient
-	c = EtcClient(**cfg['config']['etcd'])
+	c = EtcClient(loop=loop, **cfg['config']['etcd'])
 	yield from c._init()
 	return c
 
