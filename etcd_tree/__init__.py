4f743b9bfd36a390718193d077c53be3f0a838a9
Matthias Urlichs <matthias@urlichs.de>
2016-06-25 12:01:10 +0200

    * Add .lookup() to EtcAwaiter, minor bug fixes

--- etcd_tree/__init__.py
+++ etcd_tree/__init__.py
@@ -34,7 +34,7 @@ This is the core of etcTree, the object tree.
 import os
 import asyncio
 
-__VERSION__ = (0,17,4)
+__VERSION__ = (0,18,0)
 
 import warnings
 warnings.filterwarnings('ignore', category=ResourceWarning)
