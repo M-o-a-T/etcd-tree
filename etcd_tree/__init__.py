a647b6d186d16704221040bbd3fbd09b307b2458
Matthias Urlichs <matthias@urlichs.de>
2017-07-10 18:50:40 +0200

    v0.20.6

--- etcd_tree/__init__.py
+++ etcd_tree/__init__.py
@@ -34,7 +34,7 @@ This is the core of etcTree, the object tree.
 import os
 import asyncio
 
-__VERSION__ = (0,20,4)
+__VERSION__ = (0,20,6)
 
 import warnings
 warnings.filterwarnings('ignore', category=ResourceWarning)
